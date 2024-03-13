9b2809b8685b813f1d549927a3ab2dc8fbe64ed4
Luis Gil <luisfrancisco17msn@gmail.com>
2024-03-13 10:12:25 -0500

    Creación de estilos y componentes

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,28 @@
+import { useState } from "react"
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
+
+  const [categories, setCategories]= useState([ 'One Punch', 'Dragon Ball' ]);
+
+  const onAddCategory =(newCategory)=>{
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory,...categories])
+  }
+
   return (
-    <div>GifExpertApp</div>
+    <>
+      <h1>Gif Category</h1>
+
+      <AddCategory onNewCategory={onAddCategory}/>
+
+      {
+        categories.map(category => (
+          <GifGrid key={category} category={category}/>
+        ))
+      }
+      
+    </>
   )
 }
---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+
+export const AddCategory = ({onNewCategory}) => {
+
+    const [inputValue, setInputValue] = useState('')
+
+
+    const onSubmit = (e) =>{
+        e.preventDefault();
+        if (inputValue.trim().length <= 1) return;
+        onNewCategory(inputValue.trim());
+        setInputValue('')
+    }
+
+    const onChange = (e) => {
+        setInputValue(e.target.value)
+    }
+
+    return (
+    <form onSubmit={onSubmit}>
+        <input 
+            type="text" 
+            placeholder='Buscar Gif'
+            value={inputValue}
+            onChange={onChange}
+            />
+    </form>
+  )
+}
